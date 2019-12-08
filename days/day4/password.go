76408bc0c8b9b0ca1bd3f08a6528d222e4201098
Russell Shepherd <russellshepherd@pawikan.local>
2019-12-08 23:13:08 +0800

    day 4 part 2

--- days/day4/password.go
+++ days/day4/password.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"strconv"
+	"strings"
+)
+
 func ascending(a []int) bool {
 	for i := 0; i < len(a)-1; i++ {
 		if a[i] > a[i+1] {
@@ -9,10 +14,26 @@ func ascending(a []int) bool {
 	return true
 }
 
-func hasPair(a []int) bool {
+func hasAtleastOne2Pair(a []int) bool {
+	pairIndexes := make([]int, 0)
 	for i := 0; i < len(a)-1; i++ {
 		if a[i] == a[i+1] {
+			pairIndexes = append(pairIndexes, i)
+		}
+	}
+	for i := 0; i < len(pairIndexes); i++ {
+		l := len(a) - 2
+		if pairIndexes[i] >= l && a[pairIndexes[i]] != a[pairIndexes[i]-1] {
 			return true
+		}
+		if pairIndexes[i] > 0 {
+			if a[pairIndexes[i]] != a[pairIndexes[i]-1] && a[pairIndexes[i]] != a[pairIndexes[i]+2] {
+				return true
+			}
+		} else {
+			if a[pairIndexes[i]] != a[pairIndexes[i]+2] {
+				return true
+			}
 		}
 	}
 	return false
@@ -27,13 +48,32 @@ func intToSlice(n int, sequence []int) []int {
 	return sequence
 }
 
-func main() {
+func sliceToInt(a []int) (int, error) {
+	valuesText := make([]string, 0)
+	for i := range a {
+		number := a[i]
+		text := strconv.Itoa(number)
+		valuesText = append(valuesText, text)
+	}
+	return strconv.Atoi(strings.Join(valuesText, ""))
+}
+
+func findPasswords(low, high int) []int {
 	var passwords = make([]int, 0)
-	for i := 264360; i <= 746325; i++ {
-		a := intToSlice(i, []int{})
-		if ascending(a) && hasPair(a) {
-			passwords = append(a, passwords...)
+	var a = make([]int, 0)
+	for i := low; i <= high; i++ {
+		a = intToSlice(i, []int{})
+		if ascending(a) && hasAtleastOne2Pair(a) {
+			aInt, err := sliceToInt(a)
+			if err == nil {
+				passwords = append(passwords, aInt)
+			}
 		}
 	}
+	return passwords
+}
+
+func main() {
+	passwords := findPasswords(264360, 746325)
 	println("number of passwords: %d", len(passwords))
 }
--- days/day4/password_test.go
+++ days/day4/password_test.go
@@ -13,6 +13,7 @@ func TestAscending(t *testing.T) {
 		{1234, true},
 		{4321, false},
 		{123450, false},
+		{1111, true},
 	}
 	for _, c := range cases {
 		a := intToSlice(c.in, []int{})
@@ -28,17 +29,52 @@ func TestHasPair(t *testing.T) {
 		in   int
 		want bool
 	}{
-		{1234, false},
-		{4321, false},
-		{211, true},
-		{2121, false},
-		{111111, true},
+		{11122, true},
+		{1112, false},
+		{12344, true},
+		{123444, false},
 	}
 	for _, c := range cases {
 		a := intToSlice(c.in, []int{})
-		got := hasPair(a)
+		got := hasAtleastOne2Pair(a)
 		if got != c.want {
 			t.Errorf(fmt.Sprintf("hasPair(%d) = %t, want %t", c.in, got, c.want))
+		}
+	}
+}
+
+func TestSliceToInt(t *testing.T) {
+	cases := []struct {
+		in   []int
+		want int
+	}{
+		{[]int{1, 2, 3}, 123},
+		{[]int{2, 1, 5, 5}, 2155},
+	}
+	for _, c := range cases {
+		got, err := sliceToInt(c.in)
+		if err != nil {
+			println("err")
+		}
+		if got != c.want {
+			t.Errorf(fmt.Sprintf("sliceToInt(%d) = %d, want %d", c.in, got, c.want))
+		}
+	}
+}
+
+func TestFindPasswords(t *testing.T) {
+	cases := []struct {
+		low  int
+		high int
+		want int
+	}{
+		{10, 12, 1},
+	}
+	for _, c := range cases {
+		passwords := findPasswords(c.low, c.high)
+		got := len(passwords)
+		if got != c.want {
+			t.Errorf(fmt.Sprintf("findPasswords(%d, %d) = %d, want %d. passwords found: %v", c.low, c.high, got, c.want, passwords))
 		}
 	}
 }
